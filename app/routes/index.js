fde715f695d59f6428ede011ffcab4c527df68b5
Mario Linz <gelight@gmx.de>
2018-06-04 22:51:47 +0200

    Updated content...

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,15 @@
 import Route from '@ember/routing/route';
+import RSVP from 'rsvp';
+import { inject as service } from '@ember/service';
 
 export default Route.extend({
+    
+    store: service(),
+
+    model() {
+        return RSVP.hash({
+            events: this.get('store').findAll('news')
+        });
+    }
+
 });
