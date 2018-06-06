07d26467c09095d94b040d54f230b320c58ae55d
Mario Linz <gelight@gmx.de>
2018-06-06 10:59:19 +0200

    Update environment.js
    Added "ENV.locationType = 'hash';" to production properties for guthib.io.

--- config/environment.js
+++ config/environment.js
@@ -54,7 +54,7 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-    // here you can enable a production-specific feature
+    ENV.locationType = 'hash';
   }
 
   return ENV;
