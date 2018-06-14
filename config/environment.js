4bd0a014e3e17e54a7e75d28ccce4b86a96fdd17
mlinz <mario.linz@rewe-digital.com>
2018-06-14 10:42:55 +0200

    Set hash as location type

--- config/environment.js
+++ config/environment.js
@@ -5,7 +5,7 @@ module.exports = function(environment) {
     modulePrefix: 'burg-kapellendorf',
     environment,
     rootURL: '/',
-    locationType: 'auto',
+    locationType: 'hash',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
@@ -58,7 +58,7 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-    ENV.locationType = 'auto';
+    ENV.locationType = 'hash';
   }
 
   return ENV;
